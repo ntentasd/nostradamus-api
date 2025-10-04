c954729f04485fba5ec23f6918ffacefa6b6229a
Dimitris Ntentas <dimitrisntentas2002@gmail.com>
2025-10-04 20:22:05 +0300

    add response helper for method not allowed

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -6,8 +6,16 @@ import (
 	"net/http"
 )
 
+type Body map[string]any
+
 func ReplyJSON(w http.ResponseWriter, status int, body map[string]any) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(body)
+}
+
+func ReplyMethodNotAllowed(w http.ResponseWriter) error {
+	return ReplyJSON(w, http.StatusMethodNotAllowed, Body{
+		"error": "Method Not Allowed",
+	})
 }
